e68f2ed16854899f7dbbfc8191d49e63291cedf4
citizenzet <citizenzet@citizenzet>
2018-01-31 15:16:59 +0600

    +

--- analytics/Collector.php
+++ analytics/Collector.php
@@ -17,11 +17,12 @@ abstract class Collector
     public $dateTo;
     public $queries;
 
-    function  __construct($dateFrom = null, $dateTo = null, $queries = [])
+    function  __construct($dateFrom = null, $dateTo = null, $queries = [],$params = [])
     {
         $this->dateFrom = $dateFrom;
         $this->dateTo = $dateTo;
         $this->queries = $queries;
+        $this->setUp($params);
     }
     
     /**
@@ -34,5 +35,15 @@ abstract class Collector
         $queryClass = $this->queryClass;
         
         return $queryClass::query($this->dateFrom, $this->dateTo, $this->queries);
+    }
+    
+    /**
+     * для доп телодвижений
+     * 
+     * @param array $params
+     */
+    public function setUp(&$params)
+    {
+        
     }
 }
