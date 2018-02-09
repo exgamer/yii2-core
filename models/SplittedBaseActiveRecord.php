9164fe904d9ff7965e99cc1bbe8e5dc07a07adcc
citizenzet <citizenzet@citizenzet>
2018-02-09 15:08:58 +0600

    +

--- models/SplittedBaseActiveRecord.php
+++ models/SplittedBaseActiveRecord.php
@@ -1,7 +1,7 @@
 <?php
 namespace core\models;
 
-use core\traits\RemoteBaseActiveRecordTrait;
+use core\traits\SplittedBaseActiveRecordTrait;
 
 /**
  * базовая модель для данных которые частично хранятся на удаленном серваке по значению определенного аттрибута
@@ -10,5 +10,5 @@ use core\traits\RemoteBaseActiveRecordTrait;
  */
 abstract class SplittedBaseActiveRecord extends RemoteBaseActiveRecord
 {
-    use RemoteBaseActiveRecordTrait;
+    use SplittedBaseActiveRecordTrait;
 }
