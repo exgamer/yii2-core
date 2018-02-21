5bd9c09c45df39fbafc5e554281e2ccc4785d786
citizenzet <citizenzet@citizenzet>
2018-02-21 15:31:57 +0600

    +

--- data/RemoteActiveDataProvider.php
+++ data/RemoteActiveDataProvider.php
@@ -41,7 +41,7 @@ class RemoteActiveDataProvider extends ActiveDataProvider
     protected function prepareTotalCount()
     {
         $modelClass = $this->query->modelClass;
-        if ((property_exists($modelClass, 'findLocally') && $modelClass::$findLocally) || ! $this->query instanceof RemoteBaseActiveQuery || ! $this->query instanceof MixedRemoteBaseActiveQuery){
+        if ((property_exists($modelClass, 'findLocally') && $modelClass::$findLocally) || ! property_exists($modelClass, 'findLocally')){
             return parent::prepareTotalCount();
         }
         if (!$this->query instanceof QueryInterface) {
