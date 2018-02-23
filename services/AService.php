a9fdcde0c3ff23590c07419865113e8e54f99c45
i.skiba <i.skiba@192.168.2.98>
2018-02-23 16:14:16 +0600

    сервис - получение по id + сохранение

--- services/AService.php
+++ services/AService.php
@@ -2,8 +2,11 @@
 namespace core\services;
 
 use Yii;
-use yii\base\Exception;
 use yii\base\Component;
+use yii\base\ActiveRecord;
+use yii\db\ActiveRecord;
+use yii\db\Exception;
+use yii\helpers\Json;
 use yii\web\ServerErrorHttpException;
 
 /**
@@ -27,6 +30,47 @@ abstract class AService extends Component
         throw new ServerErrorHttpException(
                 Yii::t('api', 'Не определена БД для работы сервиса.')
         );
+    }
+    
+    /**
+     * Получение объекта по идентификатору
+     * 
+     * @param string $modelClass
+     * @param integer $id
+     * @param array $with
+     * 
+     * @return ActiveRecord
+     */
+    public  function getById($modelClass, $id , $with = [])
+    {       
+        $tableName = $modelClass::tableName();
+        $query = $modelClass::find();
+        if(! empty($with)){
+            $query->with($with);
+        }
+        $query->where(["{$tableName}.id" => $id]);
+
+        return $query->one();
+    }
+    
+    /**
+     * Сохранение модели
+     * 
+     * @param ActiveRecord $model
+     * @param boolean $validation
+     * @throws Exception
+     */
+    public function saveModel(ActiveRecord $model, $validation = true)
+    {
+        return static::getDb()->transaction(function($db) use($model, $validation){
+            if(! $model->save($validation)){
+                    throw new Exception(
+                            Yii::t('service','Не удалось сохранить модель - {errors}', [
+                                'errors' => Json::encode($model->getErrors())
+                            ])
+                    );
+            }
+        });
     }
     
     /**
