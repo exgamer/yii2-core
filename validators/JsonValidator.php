f8000ffbd04a3943905cf82d5506048d764145bb
citizenzet <citizenzet@citizenzet>
2018-04-02 14:47:18 +0600

    +

--- validators/JsonValidator.php
+++ validators/JsonValidator.php
@@ -7,18 +7,25 @@ class JsonValidator extends Validator
 {
     public function validateAttribute($model, $attribute)
     {
-        if (!is_array($model->$attribute)) {
-            $this->addError($model, $attribute, 'Must be array to serialize');
-            return false;
+        if (is_array($model->$attribute)){
+            $model->$attribute = json_encode($model->$attribute);
         }
-
-        $json = json_encode($model->$attribute);
-        if (!$json) {
+        if (!json_decode($model->$attribute)){
             $this->addError($model, $attribute, 'Not valid json object');
             return false;
         }
+//        if (!is_array($model->$attribute)) {
+//            $this->addError($model, $attribute, 'Must be array to serialize');
+//            return false;
+//        }
+//
+//        $json = json_encode($model->$attribute);
+//        if (!$json) {
+//            $this->addError($model, $attribute, 'Not valid json object');
+//            return false;
+//        }
 
-        $model->$attribute = $json;
+        //$model->$attribute = $json;
         return true;
 
     }
