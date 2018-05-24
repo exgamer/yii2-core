a2d4cc0b2b9c544fb60936aa3eb416bd18d22812
i.skiba <i.skiba@192.168.2.98>
2018-05-24 10:05:07 +0600

    парсинг ошибки как json - перевод в общую структуру

--- traits/ModelTrait.php
+++ traits/ModelTrait.php
@@ -1,5 +1,9 @@
 <?php
+
 namespace core\traits;
+
+use core\helpers\StringHelper;
+use yii\helpers\Json;
 
 trait ModelTrait
 {
@@ -13,7 +17,37 @@ trait ModelTrait
         if($m instanceof \Exception){
             $m = $m->getMessage();
         }
-        $this->addError('server-error' , $m);
+        $this->parseError($m);
+    }
+    
+    /**
+     * Разбирает ошибку от PDS и приводит ее к общему виду
+     * Иначе устанавливает текущую ошибку
+     * 
+     * @param string $message
+     * 
+     * @return array|null
+     */
+    protected function parseError($message)
+    {
+        if(! StringHelper::isJson($message)) {
+            $this->addError('server-error' , $message);
+            
+            return;
+        }
+        
+        $json = Json::decode($message);
+        if(! is_array($json)) {
+            $this->addError('server-error' , $message);
+            
+            return;
+        }
+        
+        foreach ($json as $error) {
+            if(! isset($error['field']) || ! isset($error['message'])) {
+                continue;
+            }
+            $this->addError($error['field'] , $error['message']);
+        }
     }
 }
-
