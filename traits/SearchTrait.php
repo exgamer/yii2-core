cae3863e9c9ff1c99215a903f7ac1e75abbe2b94
i.skiba <i.skiba@192.168.2.98>
2018-06-14 10:55:14 +0600

    фикс - проверка на существования релейшина перед передачей в with есть вероятность что геттер не является релейшином

--- traits/SearchTrait.php
+++ traits/SearchTrait.php
@@ -146,11 +146,33 @@ trait SearchTrait
             return;
         }
         foreach($extraFields as $filed) {
-            if(! $this->isExpandField($filed)) {
+            if(! $this->isExpandField($filed) || ! $this->checkRelation($filed)) {
                 continue;
             }
             $query->with($filed);
         }
+    }
+    
+    /**
+     * Проверка на существования релейшина
+     * 
+     * @param string $expand
+     * @return boolean
+     */
+    private function checkRelation($expand)
+    {
+        $method = "get{$expand}";
+        #существование метода
+        if(! method_exists($this, $method)){
+            return false;
+        }
+        $result = $this->{$method}();
+        #экземпляр ActiveQuery
+        if(! $result instanceof ActiveQuery){
+            return false;
+        }
+        
+        return true;
     }
     
     /**
